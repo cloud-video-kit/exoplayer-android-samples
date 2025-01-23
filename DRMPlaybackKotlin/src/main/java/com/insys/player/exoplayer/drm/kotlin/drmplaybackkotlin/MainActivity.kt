df594a9ce8d7c25a98199db3d8340b689a240a75
Szymon Miękus <s.miekus@insys.pl>
2025-01-23 22:48:40 +0100

    Add sample video list and update SDK versions in build.gradle

--- DRMPlaybackKotlin/src/main/java/com/insys/player/exoplayer/drm/kotlin/drmplaybackkotlin/MainActivity.kt
+++ DRMPlaybackKotlin/src/main/java/com/insys/player/exoplayer/drm/kotlin/drmplaybackkotlin/MainActivity.kt
@@ -1,40 +1,223 @@
 package com.insys.player.exoplayer.drm.kotlin.drmplaybackkotlin
 
+// Dodaj te importy
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.insys.player.exoplayer.drm.kotlin.drmplaybackkotlin.databinding.ActivityMainBinding
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.database.DatabaseProvider
+import com.google.android.exoplayer2.database.ExoDatabaseProvider
+import com.google.android.exoplayer2.drm.DefaultDrmSessionManager
+import com.google.android.exoplayer2.drm.DefaultDrmSessionManagerProvider
+import com.google.android.exoplayer2.drm.FrameworkMediaDrm
+import com.google.android.exoplayer2.drm.HttpMediaDrmCallback
+import com.google.android.exoplayer2.offline.*
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
+import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.dash.DashMediaSource
 import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource
 import com.google.android.exoplayer2.upstream.*
+import com.google.android.exoplayer2.upstream.cache.*
 import com.google.android.exoplayer2.util.MimeTypes
-import com.google.android.exoplayer2.drm.DefaultDrmSessionManager
-import com.google.android.exoplayer2.drm.ExoMediaDrm
-import com.google.android.exoplayer2.drm.FrameworkMediaDrm
-import com.google.android.exoplayer2.drm.HttpMediaDrmCallback
+import com.insys.player.exoplayer.drm.kotlin.drmplaybackkotlin.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var exoPlayer: ExoPlayer? = null
 
+    // Cache and database variables
+    private lateinit var cache: SimpleCache
+    private lateinit var databaseProvider: DatabaseProvider
+    private lateinit var downloadManager: DownloadManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
+
+        setupCache()
+        setupDownloadManager()
+
+        // Check and handle media download
+        checkAndDownloadMedia()
+    }
+
+    private fun setupCache() {
+        databaseProvider = ExoDatabaseProvider(this)
+        val cacheDir = File(this.cacheDir, "media")
+        cache = SimpleCache(
+            cacheDir,
+            LeastRecentlyUsedCacheEvictor(100L * 1024 * 1024), // 100MB cache size
+            databaseProvider
+        )
+    }
+
+    private fun setupDownloadManager() {
+        downloadManager = DownloadManager(
+            this,
+            databaseProvider,
+            cache,
+            DefaultHttpDataSource.Factory().setUserAgent(USER_AGENT)
+        )
+    }
+
+    private fun checkAndDownloadMedia() {
+        val uri = Uri.parse(URL)
+
+        // Check if media is cached
+        if (isMediaCached(cache, uri)) {
+            initializePlayer()
+        } else {
+            // Media not cached, initiate download
+            showMessage("Media not cached. Starting download.")
+            downloadMedia(uri)
+        }
+    }
+
+    private fun isMediaCached(simpleCache: SimpleCache, uri: Uri): Boolean {
+//        val cacheKey = CacheKeyFactory.DEFAULT.buildCacheKey(DataSpec(uri))
+        val cacheKey = CacheKeyFactory.DEFAULT.buildCacheKey(DataSpec(uri))
+        val cachedSpans = simpleCache.getCachedSpans(cacheKey)
+
+        var cachedLength = 0L
+        for (span in cachedSpans) {
+            cachedLength += span.length
+        }
+
+        val contentLength = simpleCache.getContentMetadata(cacheKey)
+            .get(ContentMetadata.KEY_CONTENT_LENGTH, C.LENGTH_UNSET.toLong())
+
+        return  cachedLength > 0
+//        return contentLength > 0 && cachedLength == contentLength
+    }
+
+    private fun downloadMedia(uri: Uri) {
+        val uri2 = Uri.parse(URL)
+        if (uri2.scheme == null || uri2.host == null) {
+            throw IllegalArgumentException("Invalid URI: $uri2")
+        }
+
+        val mediaId = uri2.lastPathSegment ?: uri2.toString()
+
+        val downloadRequest = DownloadRequest.Builder(mediaId, uri2).build()
+        downloadManager.addDownload(downloadRequest)
+        downloadManager.resumeDownloads()
+
+        val handler = Handler(Looper.getMainLooper())
+        val updateProgressRunnable = object : Runnable {
+            override fun run() {
+                val downloads = downloadManager.currentDownloads
+                for (download in downloads) {
+                    if (download.request.id == mediaId && download.state == Download.STATE_DOWNLOADING) {
+                        val percentDownloaded = download.percentDownloaded
+                        if (percentDownloaded != C.PERCENTAGE_UNSET.toFloat()) {
+                            showMessage("Download progress: ${percentDownloaded.toInt()}%")
+                        }
+                    }
+                }
+                handler.postDelayed(this, 5000) // Update every 5 seconds
+            }
+        }
+        handler.post(updateProgressRunnable)
+
+        downloadManager.addListener(object : DownloadManager.Listener {
+            override fun onDownloadChanged(
+                downloadManager: DownloadManager,
+                download: Download,
+                finalException: Exception?
+            ) {
+                when (download.state) {
+                    Download.STATE_COMPLETED -> {
+                        if (download.request.id == mediaId) {
+                            showMessage("Download progress: 100%")
+                            showMessage("Download completed for media: $mediaId")
+                            initializePlayer()
+                            handler.removeCallbacks(updateProgressRunnable) // Stop updates
+                        }
+                    }
+                    Download.STATE_FAILED -> {
+                        if (download.request.id == mediaId) {
+                            showError("Download failed for media: $mediaId")
+                            handler.removeCallbacks(updateProgressRunnable) // Stop updates
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    private fun initializePlayer() {
+        val uri = Uri.parse(URL)
+
+        if (!isMediaCached(cache, uri)) {
+            showError("Media not available offline.")
+            return
+        }
+
+        val cacheDataSourceFactory = CacheDataSource.Factory()
+            .setCache(cache)
+            .setUpstreamDataSourceFactory(DefaultHttpDataSource.Factory().setUserAgent(USER_AGENT))
+            .setCacheWriteDataSinkFactory(CacheDataSink.Factory().setCache(cache))
+
+        ///// Offline POC
+        val drmSessionManager: DefaultDrmSessionManager
+        val uuid = C.WIDEVINE_UUID
+        val mediaDrm = FrameworkMediaDrm.DEFAULT_PROVIDER //.newInstance(uuid)
+        val drmCallback = HttpMediaDrmCallback(DRM_LICENSE_URL, DefaultHttpDataSource.Factory().setUserAgent(USER_AGENT))
+        Log.e("dsds","dsds")
+        drmSessionManager = DefaultDrmSessionManager.Builder()
+            .setUuidAndExoMediaDrmProvider(uuid, mediaDrm)
+            .build(drmCallback)
+
+        drmSessionManager.setMode(DefaultDrmSessionManager.MODE_DOWNLOAD, null)
+        /////
+
+        val mediaSource: MediaSource = DashMediaSource.Factory(
+            DefaultDashChunkSource.Factory(cacheDataSourceFactory),
+            cacheDataSourceFactory
+        )
+            .setDrmSessionManagerProvider { drmSessionManager } // offline POC
+            .createMediaSource(MediaItem.Builder()
+            .setUri(uri)
+            .setMimeType(MimeTypes.APPLICATION_MPD)
+            .setDrmConfiguration(
+                MediaItem.DrmConfiguration.Builder(drmSchemeUuid)
+                    .setLicenseUri(DRM_LICENSE_URL)
+                    .setLicenseRequestHeaders(httpHeaders)
+//                    .setKeySetId()
+                    .build()
+            )
+            .build()
+        )
+
+        exoPlayer = ExoPlayer.Builder(this)
+            .setSeekForwardIncrementMs(10000)
+            .setSeekBackIncrementMs(10000)
+            .build()
+
+        exoPlayer?.setMediaSource(mediaSource)
+        exoPlayer?.playWhenReady = true
+        exoPlayer?.prepare()
+
+        binding.playerView.player = exoPlayer
     }
 
     private fun releasePlayer() {
-        exoPlayer?.let { player ->
-            player.release()
-            exoPlayer = null
-        }
+        exoPlayer?.release()
+        exoPlayer = null
     }
 
     override fun onStart() {
         super.onStart()
-        initializePlayer()
+        val uri = Uri.parse(URL)
+        if (isMediaCached(cache, uri)) {
+            initializePlayer()
+        }
     }
 
     override fun onStop() {
@@ -42,77 +225,24 @@ class MainActivity : AppCompatActivity() {
         releasePlayer()
     }
 
-    override fun onPause() {
-        super.onPause()
-        releasePlayer()
+    private fun showError(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        releasePlayer()
-    }
-
-    private fun initializePlayer() {
-        // Create a data source factory for HTTP requests
-        val dataSourceFactory = DefaultHttpDataSource.Factory()
-            .setUserAgent(USER_AGENT)
-            .setTransferListener(DefaultBandwidthMeter.Builder(this)
-                .setResetOnNetworkTypeChange(false)
-                .build())
-
-        // Create a DRM session manager
-        val mediaDrm = FrameworkMediaDrm.newInstance(drmSchemeUuid)
-        val drmCallback = HttpMediaDrmCallback(DRM_LICENSE_URL, dataSourceFactory)
-        httpHeaders.forEach { (key, value) ->
-            drmCallback.setKeyRequestProperty(key, value)
-        }
-        val drmSessionManager = DefaultDrmSessionManager.Builder()
-            .setUuidAndExoMediaDrmProvider(drmSchemeUuid) { mediaDrm }
-            .setMultiSession(true)
-            .build(drmCallback)
-
-        // Create a media source factory for DASH (Dynamic Adaptive Streaming over HTTP)
-        val mediaSourceFactory = DashMediaSource.Factory(DefaultDashChunkSource.Factory(dataSourceFactory),
-            DefaultHttpDataSource.Factory().setUserAgent(USER_AGENT))
-            .setDrmSessionManagerProvider { drmSessionManager }
-
-        // Create a media item with the necessary details
-        val mediaItem = MediaItem.Builder()
-            .setUri(Uri.parse(URL))  // Set the media URL
-            .setMimeType(MimeTypes.APPLICATION_MPD)  // Set the MIME type of the media
-            .setDrmConfiguration(
-                MediaItem.DrmConfiguration.Builder(drmSchemeUuid)
-                    .setLicenseUri(DRM_LICENSE_URL)  // Set the DRM license server URL
-                    .setLicenseRequestHeaders(httpHeaders)  // Set custom DRM request headers
-                    .build()
-            )
-            .build()
-
-        // Create an ExoPlayer instance using the ExoPlayer.Builder
-        ExoPlayer.Builder(this)
-            .setSeekForwardIncrementMs(10000)  // Set the seek forward increment duration
-            .setSeekBackIncrementMs(10000)  // Set the seek backward increment duration
-            .build().also { exoPlayer = it }
-
-        // Set the media source, playWhenReady, and prepare the ExoPlayer
-        exoPlayer?.setMediaSource(mediaSourceFactory.createMediaSource(mediaItem))
-        exoPlayer?.playWhenReady = true
-        exoPlayer?.prepare()
-
-        // Set the ExoPlayer instance to the player view
-        binding.playerView.player = exoPlayer
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     companion object {
-         private const val URL =
-             "https://dh7xcm7fqixq8.cloudfront.net/cf06bdcb-2db5-429f-8e7e-7b3e5d2742d7/assets/a63aa0d4-9f66-44e4-8130-94f9d5175367/videokit-576p-dash-hls-drm/dash/index.mpd"
-         private const val DRM_LICENSE_URL =
-             "https://qa-lab-tenant1.la-drm.lab.cloud.insysvt.com/acquire-license/widevine"
-         private const val USER_AGENT = "ExoPlayer-Drm"
-         private val drmSchemeUuid = C.WIDEVINE_UUID // DRM Type
-         private val httpHeaders: Map<String, String> = mutableMapOf(
-             "x-drm-brandGuid" to "cf06bdcb-2db5-429f-8e7e-7b3e5d2742d7",
-             "x-drm-userToken" to "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDU2NjI5NjcsImRybVRva2VuSW5mbyI6eyJleHAiOiIyMDI2LTAxLTA4VDA5OjQ0OjUwLjAwMDI0MCIsImtpZCI6WyIqIl0sInAiOnsicGVycyI6dHJ1ZSwiZXhjIjp7IldpZGV2aW5lQ2FuUmVuZXciOnRydWUsIldpZGV2aW5lTGljZW5zZUR1cmF0aW9uU2Vjb25kcyI6NjAsIldpZGV2aW5lUmVudGFsRHVyYXRpb25TZWNvbmRzIjo2MCwiV2lkZXZpbmVQbGF5YmFja0R1cmF0aW9uU2Vjb25kcyI6NjAsIldpZGV2aW5lUmVuZXdhbERlbGF5U2Vjb25kcyI6MTB9fX19.W-Em0Xq35JO5p0yrMxrTGfceQFCAMzjIfRLarpsqFaw"
-         )
-     }
+        private const val URL =
+            "https://dtkya1w875897.cloudfront.net/da6dc30a-e52f-4af2-9751-000b89416a4e/assets/357577a1-3b61-43ae-9af5-82b9727e2f22/videokit-720p-dash-hls-drm/dash/index.mpd"
+        private const val DRM_LICENSE_URL =
+            "https://insys-marketing.la.drm.cloud/acquire-license/widevine?brandguid=da6dc30a-e52f-4af2-9751-000b89416a4e&usertoken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE4OTM0NTYwMDAsImRybVRva2VuSW5mbyI6eyJleHAiOiIyMDMwLTAxLTAxVDAwOjAwOjAwKzAwOjAwIiwia2lkIjpbIjFmODNhZTdmLTMwYzgtNGFkMC04MTcxLTI5NjZhMDFiNjU0NyJdLCJwIjp7InBlcnMiOmZhbHNlfX19.hElVqrfK-iLeV_ZleJJO8i-Mf1D2yYVXdtgBE0ja9R4"
+        private const val USER_AGENT = "ExoPlayer-Drm"
+        private val drmSchemeUuid = C.WIDEVINE_UUID // DRM Type
+        private val httpHeaders: Map<String, String> = mutableMapOf(
+            "x-drm-brandGuid" to "da6dc30a-e52f-4af2-9751-000b89416a4e",
+            "x-drm-userToken" to "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE4OTM0NTYwMDAsImRybVRva2VuSW5mbyI6eyJleHAiOiIyMDMwLTAxLTAxVDAwOjAwOjAwKzAwOjAwIiwia2lkIjpbIjFmODNhZTdmLTMwYzgtNGFkMC04MTcxLTI5NjZhMDFiNjU0NyJdLCJwIjp7InBlcnMiOmZhbHNlfX19.hElVqrfK-iLeV_ZleJJO8i-Mf1D2yYVXdtgBE0ja9R4",
+        )
+    }
 }
